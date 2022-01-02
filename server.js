49bc3df5b2cebe99daa23c15e8c63865f504dd28
Michel Christophe <mchristopheweb@gmail.com>
2022-01-02 11:52:21 +0100

    ajout de ditenv et du fichier .env

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-console.log("start server.js");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT;
 const http = require('http');
 
 const app = require("./app");
 
 
 app.get("/", (req, res)=>{
+    
     res.send("ok ça marche")
 })
-app.listen(3000);
+app.listen(PORT || 3000, ()=>{
+    
+    console.log("serveur à l'écoute sur le port "+ PORT)
+});
