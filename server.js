946dda3b883b290ba31b094154f3ac7c74f55303
Michel Christophe <mchristopheweb@gmail.com>
2022-01-02 16:06:01 +0100

    mise en place de la base de données

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 const dotenv = require("dotenv");
 dotenv.config();
+
 const PORT = process.env.PORT;
+const PATHDB = process.env.PATHDB;
 const http = require('http');
+const mongoose = require('mongoose');
+
+/***************************************************** */
+//  CONNECTION À LA BASE DE DONNÉES MONGODB
+/***************************************************** */
+
+
+mongoose.connect(PATHDB, { useNewUrlParser: true, useUnifiedTopology: true })
+        .then(() => console.log('Connexion à MongoDB réussie !'))
+        .catch((err) => {console.log('Connexion à MongoDB échouée !'); console.log(err);});
+
+
+/***************************************************** */
+//  FIN DE CONNECTION À LA BASE DE DONNÉES MONGODB
+/***************************************************** */
 
 const app = require("./app");
 
